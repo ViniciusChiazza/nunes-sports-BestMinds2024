d741657e946d5fbc74538682d81b06ae9718e91d
ViniciusChiazza <viniciuschiazza@gmail.com>
2024-01-23 00:05:56 -0300

    commit com algumas modificaçoes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
 
 const app = express();
 const port = 3000;
+
+
+app.use(cors());
 
 // Servir arquivos estáticos a partir do diretório 'public'
 app.use(express.static('public'));
